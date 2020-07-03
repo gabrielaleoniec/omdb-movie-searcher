84401b37477c1556d160aacff065f3d096cfdc42
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-07-03 07:50:52 +0200

    Styles for the Movies

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import './Movie.css'
+
+const Poster = (poster, title) => {
+    if(poster === "N/A") {
+        return <div className="Movie-image"><i class="fa fa-image fa-3x Movie-icon"></i></div>
+    }
+    return <img src={poster} alt={title} className="Movie-image"/>
+}
 
 const Movie = (props) => {
-    const {key, image} = props
+    const {key, movie} = props
 
-    return <div key={key}>
-        <img src={image.Poster}/>
-        <h3>{image.Title}</h3>
-        <div>{image.Type}</div>
-        <div>{image.Year}</div>
-
-        {image.Year}
+    return <div key={key} className="Movie">
+        {Poster(movie.Poster, movie.Title)}
+        <div className="Movie-cover"></div>
+        <div className="Movie-data">
+            <h3 className="Movie-title">{movie.Title}</h3>
+            <div>{movie.Type}</div>
+            <div>{movie.Year}</div>
+        </div>
     </div>
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Movie from './Movie'
+import './MovieList.css'
 
 const MovieList = (props) => {
-    const images = props.images.map((image) =>  <Movie key={image.id} image={image} />)
+    if(props.movies === null) {
+        return null
+    }
+
+    const movies = props.movies.map((movie) =>  <Movie key={movie.id} movie={movie} />)
+
+    if(!props.movies.length) {
+        return <h2 className="Movies-info">No movies found</h2>
+    }
 
     return <section>
-        {props.images.length} images found
-        <ul>
-            {images}
+        <div>
+        <h2 className="Movies-info">{props.movies.length} {props.movies.length === 1? ('movie'): ('movies')} listed</h2>
+        <ul className="Movies">
+            {movies}
         </ul>
+        </div>
         </section>
 }
 
