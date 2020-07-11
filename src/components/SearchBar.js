11ac42c3c02fa5f39c5b4a7620997f2861f79f8a
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-07-11 23:45:56 +0200

    Search Bar state moved to Redux

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,51 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {titleInput} from '../actions'
 import './SearchBar.css'
 
 class SearchBar extends React.Component {
-    state = {
-        term: ''
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            query: ''
+        }
     }
 
     onFormSubmit = (e) => {
         e.preventDefault()
 
-        this.props.onSubmit(this.state.term, this.state.page)
+        this.props.dispatch(titleInput(this.state.query))
+
+        this.props.onSubmit()
     }
 
     render() {
         return (
-        <div>
-            <form onSubmit={this.onFormSubmit}>
-                <div>
+            <div>
+                <form onSubmit={this.onFormSubmit}>
                     <div>
-                    <label className="Search-label" htmlFor="search">Movie title</label>
-                    <input
-                        id="search"
-                        className="Search-input"
-                        type="text"
-                        placeholder="Movie title..."
-                        value={this.state.term}
-                        onChange={e => this.setState({term: e.target.value})}></input>
+                        <div>
+                        <label className="Search-label" htmlFor="search">Movie title</label>
+                        <input
+                            id="search"
+                            className="Search-input"
+                            type="text"
+                            placeholder="Movie title..."
+                            value={this.state.query}
+                            onChange={e => this.setState({query: e.target.value})}></input>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
     }
 }
 
-export default SearchBar;
+const mapStateToProps = (state) => {
+    return {
+        query: state.query
+    }
+}
+
+export default connect(mapStateToProps)(SearchBar);
