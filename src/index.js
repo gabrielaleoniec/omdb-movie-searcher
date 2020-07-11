07a483357ee4c0afb128626921f0e05a037505b1
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-07-11 17:05:11 +0200

    Redux configured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
 import './index.css';
+
 import App from './App';
+import combineReducers from './reducers'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(combineReducers)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
