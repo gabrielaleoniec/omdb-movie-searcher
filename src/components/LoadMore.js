3fb2b99cf19aa82590a4e7ada7dd29f61c2fb770
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-07-12 18:31:06 +0200

    LoadMore refactored into functional component

--- src/components/LoadMore.js
+++ src/components/LoadMore.js
@@ -1,37 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import {pageInput} from '../actions'
+import { pageInput } from '../actions'
 
-class LoadMore extends React.Component {
-    pageNrIncrease = (e) => {
+const LoadMore = (props) => {
+    const pageNrIncrease = (e) => {
         e.preventDefault()
 
-        console.log('Props page inside LoadMore', this.props.page)
+        const newPage = props.page + 1
+        props.dispatch(pageInput(newPage))
 
-        const newPage = this.props.page + 1
-
-        this.setState({page: newPage})
-
-        this.props.dispatch(pageInput(newPage))
-
-        this.props.onClick()
+        props.onClick()
     }
 
-    render () {
-        console.log('Render:', this.props)
-        if(!this.props.showMore){
-            return null
-        }
-        return <button onClick={this.pageNrIncrease}>Load more</button>
+
+    if (!props.showMore) {
+        return null
     }
+
+    return <button onClick={pageNrIncrease}>Load more</button>
 }
 
 const mapStateToProps = (state) => {
-    console.log('mapStateToProps in the Load More:', state)
     return {
         page: state.page,
         showMore: state.showMore
     }
-  }
+}
 
 export default connect(mapStateToProps)(LoadMore);
