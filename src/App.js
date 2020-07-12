5948008ecd3961c5f2aafb956ca2b121f2133583
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-07-12 18:48:52 +0200

    App refactored, changed to functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux'
-import {listMovies, toggleMore} from './actions'
+import { listMovies, toggleMore } from './actions'
 import store from './store/store'
 import './App.css';
 import SearchBar from './components/SearchBar'
 import MovieList from './components/MovieList'
 import LoadMore from './components/LoadMore'
 
+const url = 'http://www.omdbapi.com/'
+const apikey = '157f34ed'
 /**
  * TODO: One more input: year
  * TODO: Comments
@@ -15,72 +17,58 @@ import LoadMore from './components/LoadMore'
  * TODO: Add sorting
  * TODO: Add loading
  */
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      movies: null,
-      page: 1
-    }
-  }
+const App = (props) => {
+    const onSearchSubmit = async () => {
+        console.log('>>App', props)
+        // Props were not updated by mapStateToProps
+        const { title, page, movies, showMore } = store.getState()
 
+        const response = await axios.get(url, {
+            params: {
+                apikey,
+                s: title,
+                page: page || 1
+            }
+        })
 
-  onSearchSubmit = async () => {
-    console.log('>>App', this.props)
-    // Props were not updated by mapStateToProps
-    const {title, page, movies, showMore} = store.getState()
-    const url = 'http://www.omdbapi.com/'
-    const apikey = '157f34ed'
-    const response = await axios.get(url, {
-      params: {
-        apikey,
-        s: title,
-        page: page || 1
-      }
-    })
+        if (!response.data) {
+            console.log('No response data returned from server')
+        }
 
-    if(!response.data) {
-      console.log('No response data returned from server')
-    }
+        const newMovies = response.data.Search || []
 
-    console.log('Response data', response.data.Search)
-    const newMovies = response.data.Search || []
+        if (movies.length && newMovies.length < 10) {
+            props.dispatch(toggleMore(false))
+        }
+        if (newMovies.length === 10 && !showMore) {
+            props.dispatch(toggleMore(true))
+        }
 
-    if(movies.length && newMovies.length < 10){
-      this.props.dispatch(toggleMore(false))
-    }
-    if(newMovies.length === 10 && !showMore) {
-      this.props.dispatch(toggleMore(true))
+        props.dispatch(listMovies([...movies, ...newMovies]))
     }
 
-    this.props.dispatch(listMovies([...movies, ...newMovies]))
-    this.setState({ movies: [...movies, ...newMovies] })
-  }
-
-  render() {
     return (
-      <div className="App">
-          <header className="App-header">
-            <h1>Open Movie Database Search</h1>
-          </header>
-          <main>
-            <SearchBar onSubmit={this.onSearchSubmit} />
-            <MovieList />
-            <LoadMore onClick={this.onSearchSubmit} />
-          </main>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <h1>Open Movie Database Search</h1>
+            </header>
+            <main>
+                <SearchBar onSubmit={onSearchSubmit} />
+                <MovieList />
+                <LoadMore onClick={onSearchSubmit} />
+            </main>
+        </div>
     );
-  }
 }
 
 const mapStateToProps = (state) => {
-  console.log('mapStateToProps in the App:', state)
-  return {
-      title: state.title,
-      page: state.page,
-      movies: state.movies,
-      showMore: state.showMore
-  }
+    console.log('mapStateToProps in the App:', state)
+    return {
+        title: state.title,
+        page: state.page,
+        movies: state.movies,
+        showMore: state.showMore
+    }
 }
 
-export default connect(mapStateToProps, null)(App);
+export default connect(mapStateToProps)(App);
